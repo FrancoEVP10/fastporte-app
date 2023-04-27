a200fe60f1317ccf0668cb48e521264389168234
FrancoEVP10 <vasquezemmanuel439@gmail.com>
2023-04-27 11:44:20 -0500

    feature(auth): widgets implementation

--- lib/widgets/auth_background.dart
+++ lib/widgets/auth_background.dart
@@ -11,7 +11,7 @@ class AuthBackground extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       width: double.infinity,
       height: double.infinity,
       child: Stack(
@@ -34,8 +34,8 @@ class _HeaderIcon extends StatelessWidget {
     return SafeArea(
       child: Container(
         width: double.infinity,
-        margin: EdgeInsets.only( top: 30 ),
-        child: Icon( Icons.person_pin, color: Colors.white, size: 100 ),
+        margin: EdgeInsets.only( top: 60),
+        child: Image.asset('assets/imgs/logo.png', height: 100),
       ),
     );
   }
@@ -68,8 +68,8 @@ class _PurpleBox extends StatelessWidget {
   BoxDecoration _purpleBackground() => BoxDecoration(
     gradient: LinearGradient(
       colors: const [
-        Color.fromRGBO(63, 63, 156, 1),
-        Color.fromRGBO(90, 70, 178, 1)
+        Color.fromRGBO(15, 21, 163, 1),
+        Color.fromRGBO(42, 28, 185, 1),
       ]
     )
   );
