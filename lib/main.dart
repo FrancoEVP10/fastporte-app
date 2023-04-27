0b17dba598530593299bc1bafafdc3deb5c05855
FrancoEVP10 <vasquezemmanuel439@gmail.com>
2023-04-27 11:52:05 -0500

    feature(auth): ui and start screen configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,24 @@
+import 'package:fastporte_app/screens/screens.dart';
+import 'package:fastporte_app/services/services.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(AppState());
+
+class AppState extends StatelessWidget {
+  const AppState({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: ( _ ) => AuthService() ),
+      ],
+      child: MyApp(),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -10,22 +28,26 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'FastPorte',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('FastPorte App'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      initialRoute: 'start',
+      routes: {
+        
+        //'checking': ( _ ) => CheckAuthScreen(),
+
+        //'home'    : ( _ ) => HomeScreen(),
+        'start'   : ( _ ) => StartScreen(),
+        'login'   : ( _ ) => LoginScreen(),
+        'register': ( _ ) => RegisterScreen(),
+        'forgotpassword': ( _ ) => ForgotScreen(),
+      },
+      scaffoldMessengerKey: NotificationsService.messengerKey,
       theme: ThemeData.light().copyWith(
         scaffoldBackgroundColor: Colors.grey[300],
         appBarTheme: AppBarTheme(
           elevation: 0,
-          color: Colors.indigo
+          color: Color.fromRGBO(15, 21, 163, 1)
         ),
         floatingActionButtonTheme: FloatingActionButtonThemeData(
-          backgroundColor: Colors.indigo,
+          backgroundColor: Color.fromRGBO(15, 21, 163, 1),
           elevation: 0
         )
       ),
