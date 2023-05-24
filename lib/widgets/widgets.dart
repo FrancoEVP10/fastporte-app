802f69b2872fc7b89d4b81bbaeaa1f1529329746
Sebastian Bran <sebastianbranzapata@gmail.com>
2023-05-24 01:48:08 -0500

    [feature/contracts]: Add new packages

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,2 +1,6 @@
 export 'package:fastporte_app/widgets/card_container.dart';
 export 'package:fastporte_app/widgets/auth_background.dart';
+export 'package:fastporte_app/widgets/contract_card.dart';
+export 'package:fastporte_app/widgets/amount_row.dart';
+export 'package:fastporte_app/widgets/details_row.dart';
+export 'package:fastporte_app/widgets/summary_row.dart';
