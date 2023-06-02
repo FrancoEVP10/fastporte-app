2adb54fe92d10365fa1f30cfb576ce4748f20645
Jherico Solier <74070374+JhericoS@users.noreply.github.com>
2023-06-01 23:08:00 -0500

    fix error

--- lib/search/screens/search_screen.dart
+++ lib/search/screens/search_screen.dart
@@ -38,99 +38,128 @@ class _SearchScreenState extends State<SearchScreen> {
       appBar: AppBar(
         title: const Text('Search Vehicle'),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              'Filtros de búsqueda',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-          ),
-          ListTile(
-            title: const Text('Tipo de Servicio'),
-            subtitle: DropdownButton<String>(
-              value: selectedTipoServicio,
-              onChanged: (String? newValue) {
-                setState(() {
-                  selectedTipoServicio = newValue!;
-                });
-              },
-              items: <String>['carga', 'mudanza']
-                  .map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Container(
-                    width: double.infinity,
-                    alignment: Alignment.centerLeft,
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    child: Text(value),
-                  ),
-                );
-              }).toList(),
-            ),
-          ),
-          ListTile(
-            title: const Text('Tamaño de Vehículo'),
-            subtitle: DropdownButton<String>(
-              value: selectedTamanoVehiculo,
-              onChanged: (String? newValue) {
-                setState(() {
-                  selectedTamanoVehiculo = newValue!;
-                });
-              },
-              items: <String>['grande', 'pequeño']
-                  .map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-            ),
-          ),
-          ListTile(
-            title: const Text('Documentación Completa'),
-            subtitle: DropdownButton<String>(
-              value: selectedDocumentacion,
-              onChanged: (String? newValue) {
-                setState(() {
-                  selectedDocumentacion = newValue!;
-                });
-              },
-              items: <String>['si', 'no']
-                  .map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-            ),
-          ),
-          Container(
-            width: double.infinity,
-            margin: const EdgeInsets.all(16),
-            child: ElevatedButton(
-              onPressed: () {
-                // Logic to perform search based on selected filters
-                // You can use the values of selectedTipoServicio,
-                // selectedTamanoVehiculo, and selectedDocumentacion to filter the list
-                // of results and display only the desired items.
-              },
-              child: const Text(
-                'Buscar',
-                style: TextStyle(color: Colors.white),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'Filtros de búsqueda',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.purple[500],
-                shape: RoundedRectangleBorder(
+            ),
+            ListTile(
+              title: const Text('Tipo de Servicio'),
+              subtitle: Container(
+                decoration: BoxDecoration(
+                  color: Colors.purple[100],
                   borderRadius: BorderRadius.circular(8),
+                ),
+                child: DropdownButton<String>(
+                  value: selectedTipoServicio,
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      selectedTipoServicio = newValue!;
+                    });
+                  },
+                  items: <String>['carga', 'mudanza']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Container(
+                        alignment: Alignment.centerLeft,
+                        padding: const EdgeInsets.symmetric(horizontal: 16),
+                        child: Text(value),
+                      ),
+                    );
+                  }).toList(),
+                  underline: Container(),
                 ),
               ),
             ),
-          ),
-          const SizedBox(height: 16),
-          Expanded(
-            child: ListView.builder(
+            ListTile(
+              title: const Text('Tamaño de Vehículo'),
+              subtitle: Container(
+                decoration: BoxDecoration(
+                  color: Colors.purple[100],
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: DropdownButton<String>(
+                  value: selectedTamanoVehiculo,
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      selectedTamanoVehiculo = newValue!;
+                    });
+                  },
+                  items: <String>['grande', 'pequeño']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Container(
+                        alignment: Alignment.centerLeft,
+                        padding: const EdgeInsets.symmetric(horizontal: 16),
+                        child: Text(value),
+                      ),
+                    );
+                  }).toList(),
+                  underline: Container(),
+                ),
+              ),
+            ),
+            ListTile(
+              title: const Text('Documentación Completa'),
+              subtitle: Container(
+                decoration: BoxDecoration(
+                  color: Colors.purple[100],
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: DropdownButton<String>(
+                  value: selectedDocumentacion,
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      selectedDocumentacion = newValue!;
+                    });
+                  },
+                  items: <String>['si', 'no']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Container(
+                        alignment: Alignment.centerLeft,
+                        padding: const EdgeInsets.symmetric(horizontal: 16),
+                        child: Text(value),
+                      ),
+                    );
+                  }).toList(),
+                  underline: Container(),
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(16),
+              child: ElevatedButton(
+                onPressed: () {
+                  // Logic to perform search based on selected filters
+                  // You can use the values of selectedTipoServicio,
+                  // selectedTamanoVehiculo, and selectedDocumentacion to filter the list
+                  // of results and display only the desired items.
+                },
+                child: const Text(
+                  'Buscar',
+                  style: TextStyle(color: Colors.white),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.purple[700],
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+            ListView.builder(
+              shrinkWrap: true,
               itemCount: results.length,
               itemBuilder: (context, index) {
                 final result = results[index];
@@ -147,8 +176,8 @@ class _SearchScreenState extends State<SearchScreen> {
                 );
               },
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
