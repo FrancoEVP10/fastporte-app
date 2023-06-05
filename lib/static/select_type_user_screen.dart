d73a288896e4884d0eae7d9c7706043cdccaf6a1
FrancoEVP10 <vasquezemmanuel439@gmail.com>
2023-06-04 23:00:47 -0500

    refactor: redesign of the user type select screen

--- lib/static/select_type_user_screen.dart
+++ lib/static/select_type_user_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fastporte_app/globals.dart' as globals;
+import 'package:google_fonts/google_fonts.dart';
 
 class SelectRoleScreen extends StatelessWidget {
   const SelectRoleScreen({super.key});
@@ -7,72 +8,130 @@ class SelectRoleScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-            image: DecorationImage(
-          image: AssetImage('assets/imgs/fondo.jpg'),
-          fit: BoxFit.fill,
-        )),
-        child: SafeArea(
-          child: Center(
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 50,
-                ),
-                Text(
-                  'Iniciar Sesión como',
-                  style: TextStyle(
-                    fontSize: 35,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Image.asset('assets/imgs/forgot-password.png'),
-                SizedBox(
-                  height: 20,
-                ),
-                MaterialButton(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10)),
-                    elevation: 0,
-                    color: Color.fromRGBO(26, 204, 141, 1),
-                    onPressed: () {
-                      globals.role = 'cliente';
-                      Navigator.pushReplacementNamed(context, 'login');
-                    },
-                    child: Container(
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 70, vertical: 15),
-                        child: Text('Cliente',
-                            style:
-                                TextStyle(color: Colors.white, fontSize: 20)))),
-                SizedBox(
-                  height: 15,
-                ),
-                MaterialButton(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10)),
-                    elevation: 0,
-                    color: Color.fromRGBO(26, 204, 141, 1),
-                    onPressed: () {
-                      globals.role = 'transportista';
-                      Navigator.pushReplacementNamed(context, 'login');
-                    },
-                    child: Container(
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-                        child: Text('Transportista',
-                            style:
-                                TextStyle(color: Colors.white, fontSize: 20)))),
-              ],
+      body: Stack(
+        children: [
+          _BodyScreen(),
+          Opacity(
+            opacity: 0.5,
+            child: ClipPath(
+              clipper: WaveClipper(),
+              child: Container(
+                height: 200,
+                color: Color.fromRGBO(26, 204, 141, 1),
+              ),
             ),
           ),
+          ClipPath(
+            clipper: WaveClipper(),
+            child: Container(
+              height: 180,
+              color: Color.fromRGBO(26, 204, 141, 1),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _BodyScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Color.fromRGBO(15, 21, 163, 1),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            Text(
+              'FastPorte',
+              style: GoogleFonts.openSans(
+                fontSize: 35,
+                color: Colors.white,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              width: 280,
+              child: Text(
+                'La plataforma donde podrás encontrar el serivico que necesitas.',
+                style: GoogleFonts.openSans(
+                  fontSize: 18,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w500,
+                ),
+                textAlign: TextAlign.justify,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            MaterialButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50)),
+                elevation: 0,
+                color: Color.fromRGBO(26, 204, 141, 1),
+                onPressed: () {
+                  globals.role = 'cliente';
+                  Navigator.pushReplacementNamed(context, 'login');
+                },
+                child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 75, vertical: 15),
+                    child: Text('SOY CLIENTE',
+                        style: TextStyle(color: Colors.white, fontSize: 18)))),
+            SizedBox(
+              height: 15,
+            ),
+            MaterialButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50)),
+                elevation: 0,
+                color: Color.fromRGBO(26, 204, 141, 1),
+                onPressed: () {
+                  globals.role = 'transportista';
+                  Navigator.pushReplacementNamed(context, 'login');
+                },
+                child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                    child: Text('SOY TRANSPORTISTA',
+                        style: TextStyle(color: Colors.white, fontSize: 18)))),
+          ],
         ),
       ),
     );
+  }
+}
+
+class WaveClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    debugPrint(size.width.toString());
+    final path = Path();
+    path.lineTo(0, size.height);
+    final firstStart = Offset(size.width / 5, size.height);
+    final firstEnd = Offset(size.width / 2.25, size.height - 50.0);
+    path.quadraticBezierTo(
+        firstStart.dx, firstStart.dy, firstEnd.dx, firstEnd.dy);
+    final secondStart =
+        Offset(size.width - (size.width / 3.24), size.height - 105);
+    final secondEnd = Offset(size.width, size.height - 10);
+    path.quadraticBezierTo(
+        secondStart.dx, secondStart.dy, secondEnd.dx, secondEnd.dy);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return true;
   }
 }
