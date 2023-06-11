028b94bd1b5af921984b2630455b8252d1cc8f98
FrancoEVP10 <vasquezemmanuel439@gmail.com>
2023-06-11 17:52:48 -0500

    refactor: redesign user card info

--- lib/widgets/user_card_information.dart
+++ lib/widgets/user_card_information.dart
@@ -1,5 +1,6 @@
 import 'package:fastporte_app/comments/screens/driver_info.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class UserCardInformation extends StatelessWidget {
   const UserCardInformation({super.key});
@@ -9,12 +10,12 @@ class UserCardInformation extends StatelessWidget {
     return Center(
       child: Card(
         shape: RoundedRectangleBorder(
-          borderRadius: const BorderRadius.all(Radius.circular(12))
-        ),
+            borderRadius: const BorderRadius.all(Radius.circular(12))),
         child: InkWell(
           splashColor: Colors.blue.withAlpha(30),
           onTap: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context)  => DriverInfoScreen()));
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => DriverInfoScreen()));
           },
           child: SizedBox(
             width: 360,
@@ -31,14 +32,22 @@ class UserCardInformation extends StatelessWidget {
                   ),
                 ),
                 Padding(
-                  padding: EdgeInsets.only(left: 30, bottom: 20, right: 16, top: 20),
+                  padding:
+                      EdgeInsets.only(left: 30, bottom: 20, right: 16, top: 20),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.spaceAround,
                     mainAxisSize: MainAxisSize.max,
-                    children: const [
+                    children: [
                       Text('Nombres y Apellidos'),
                       Text('Vehiculo'),
-                      Text('Numero de Estrellas')
+                      RatingBarIndicator(
+                        rating: 3.0,
+                        itemSize: 25.0,
+                        itemBuilder: (context, _) => Icon(
+                          Icons.star,
+                          color: Colors.amber,
+                        ),
+                      ),
                     ],
                   ),
                 )
