70ebc5c6fedbc11bcff38bf92d7eb5f847dae6f5
FrancoEVP10 <vasquezemmanuel439@gmail.com>
2023-06-30 12:02:27 -0500

    feat(vehicle): update widget

--- lib/widgets/user_card_information.dart
+++ lib/widgets/user_card_information.dart
@@ -1,9 +1,11 @@
 import 'package:fastporte_app/comments/screens/driver_info.dart';
+import 'package:fastporte_app/vehicle/model/vehicle.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class UserCardInformation extends StatelessWidget {
-  const UserCardInformation({super.key});
+  final Vehicle vehicle;
+  const UserCardInformation({super.key, required this.vehicle});
 
   @override
   Widget build(BuildContext context) {
@@ -15,31 +17,48 @@ class UserCardInformation extends StatelessWidget {
           splashColor: Colors.blue.withAlpha(30),
           onTap: () {
             Navigator.push(context,
-                MaterialPageRoute(builder: (context) => DriverInfoScreen()));
+                MaterialPageRoute(builder: (context) => DriverInfoScreen(driver: vehicle.driver)));
           },
           child: SizedBox(
             width: 360,
             height: 110,
             child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: ClipOval(
-                    child: Image.asset(
-                      'assets/imgs/user-vector.png',
-                      height: 80,
+                if (vehicle.driver.photo == '')
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: ClipOval(
+                      child: Image.asset(
+                        'assets/imgs/user-vector.png',
+                        height: 80,
+                      ),
                     ),
                   ),
-                ),
+                if (vehicle.driver.photo != '')
+                  SizedBox(
+                    height: 80,
+                    width: 80,
+                    child: CircleAvatar(
+                      radius: 20,
+                      backgroundImage: NetworkImage(vehicle.driver.photo),
+                      backgroundColor: Colors.grey,
+                    ),
+                  ),
                 Padding(
-                  padding:
-                      EdgeInsets.only(left: 30, bottom: 20, right: 16, top: 20),
+                  padding: EdgeInsets.only(bottom: 20, right: 16, top: 20),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.spaceAround,
                     mainAxisSize: MainAxisSize.max,
                     children: [
-                      Text('Nombres y Apellidos'),
-                      Text('Vehiculo'),
+                      Text(
+                        "${vehicle.driver.name} ${vehicle.driver.lastname}",
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      Text(
+                        vehicle.brand,
+                        style: TextStyle(fontSize: 18),
+                      ),
                       RatingBarIndicator(
                         rating: 3.0,
                         itemSize: 25.0,
